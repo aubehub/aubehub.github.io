66e23bff54e29d64eaace5e3b00977e99e23cfe1
aubehub <albalopsanchez@gmail.com>
2020-05-17 18:39:18 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
+import { HomePage } from './components/HomePage';
+import { ProjectsPage } from './components/ProjectsPage';
+import { ContactPage } from './components/ContactPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyLink = (props) => {
+  const match = useRouteMatch({
+    path: props.route,
+    exact: true,
+  });
+  return <Link className={ match ? "active" : ""} to={props.route}>{props.name}</Link>;
 }
 
-export default App;
+export default function App() {
+
+  return (
+    <Router>
+      <div className="frame">
+        <div className="bar">
+          <nav>
+            <ul>
+              <li>
+                <MyLink name="Home" route="/" />
+              </li>
+              <li>
+                <MyLink name="Projects" route="/projects" />
+              </li>
+              <li>
+                <MyLink name="Contact" route="/contact" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <div className="main">
+          <Switch>
+            <Route path="/projects">
+              <ProjectsPage />
+            </Route>
+            <Route path="/contact">
+              <ContactPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+        <footer></footer>
+      </div>
+    </Router>
+  );
+}
